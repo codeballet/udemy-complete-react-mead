bd148dba2e27d9a53e9ebd664f20734ec071b027
johan <stjernholm@msn.com>
2020-03-05 22:47:37 +0800

    3.12 Conditional Rendering in JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,10 +1,81 @@
-console.log('App.js is running!')
+'use strict';
 
-// JSX - JavaScript XML
-// var template = <p>This is JSX from app.js!</p>
-var template = React.createElement("p", {
-  id: "someid"
-}, "This is JSX")
-var appRoot = document.getElementById('app')
+console.log('App.js is running!');
 
-ReactDOM.render(template, appRoot)
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer!',
+  options: ['One', 'Two']
+};
+
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item One'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item Two'
+    )
+  )
+);
+
+var user = {
+  name: 'Johan',
+  age: '47',
+  location: 'Beijing'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
