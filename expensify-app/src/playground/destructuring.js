8910877eace3af4b6eb301a19ba3596188215ac4
johan <stjernholm@msn.com>
2020-03-15 18:00:48 +0800

    10.89 ES6 Array Destructuring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object destructuring
+//
+
 // const person = {
 //   name: 'Johan',
 //   age: 42,
@@ -15,13 +19,26 @@
 //   console.log(`It's ${temperature} in ${city}`)
 // }
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-}
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// }
 
-const { name: publisherName = 'Self-Published' } = book.publisher
-console.log(publisherName)
+// const { name: publisherName = 'Self-Published' } = book.publisher
+// console.log(publisherName)
+
+
+//
+// Array destructuring
+//
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147']
+const [, city, state = 'New York'] = address
+console.log(`Your are in ${city} ${state}`)
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75']
+const [drink, priceS, priceM, priceL] = item
+console.log(`A medium ${drink} costs ${priceM}`)
