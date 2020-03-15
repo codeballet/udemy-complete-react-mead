6559aa2f6973fbb7d2043302f8fa1bbda4d4ae72
johan <stjernholm@msn.com>
2020-03-15 11:58:13 +0800

    9.80 Organizing our routes

---
+++ expensify-app/src/routers/AppRouter.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
+import AddExpensePage from '../components/AddExpensePage'
+import EditExpensePage from '../components/EditExpensePage'
+import HelpPage from '../components/HelpPage'
+import NotFoundPage from '../components/NotFoundPage'
+import Header from '../components/Header'
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route exact path="/">
+        <ExpenseDashboardPage />
+      </Route>
+      <Route path="/create">
+        <AddExpensePage />
+      </Route>
+      <Route path="/edit">
+        <EditExpensePage />
+      </Route>
+      <Route path="/help">
+        <HelpPage />
+      </Route>
+      <Route>
+        <NotFoundPage />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+)
+
+export default AppRouter
