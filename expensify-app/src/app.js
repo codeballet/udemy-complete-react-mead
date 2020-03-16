5ec78d7b38e06a3b7fefa195c26e62134c8b0d98
johan <stjernholm@msn.com>
2020-03-16 19:38:06 +0800

    11.99 Organizing Redux

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
+
+import getVisibleExpenses from './selectors/expenses'
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+
+const store = configureStore()
+
+console.log(store.getState())
+
+store.subscribe(() => {
+  const state = store.getState()
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+  console.log(visibleExpenses)
+})
+
+store.dispatch(addExpense({ description: 'Water bill' }))
+store.dispatch(addExpense({ description: 'Gas bill' }))
+store.dispatch(setTextFilter('gas'))
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
