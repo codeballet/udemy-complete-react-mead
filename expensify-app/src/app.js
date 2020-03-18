d3639bc4aac0790a9760e8fa9ffd732d48f7fb75
johan <stjernholm@msn.com>
2020-03-18 12:20:11 +0800

    11.104 Dropdown for picking sortBy

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -12,8 +12,6 @@ import { setTextFilter } from './actions/filters'
 
 const store = configureStore()
 
-console.log(store.getState())
-
 store.subscribe(() => {
   const state = store.getState()
   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
@@ -21,8 +19,8 @@ store.subscribe(() => {
 })
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }))
-store.dispatch(addExpense({ description: 'Gas bill' }))
-// store.dispatch(setTextFilter('gas'))
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }))
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }))
 
 const jsx = (
   <Provider store={store}>
