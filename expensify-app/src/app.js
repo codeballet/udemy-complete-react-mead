5873e413b76aea852d8b018cd0021d275adcf9fe
johan <stjernholm@msn.com>
2020-03-24 23:45:51 +0800

    12.121 Mocking libraries with Jest

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -6,6 +6,7 @@ import configureStore from './store/configureStore'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/initialize'
+import 'react-dates/lib/css/_datepicker.css'
 
 import getVisibleExpenses from './selectors/expenses'
 import { addExpense } from './actions/expenses'
